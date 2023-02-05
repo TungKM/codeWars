969245944d51b6d72754227319279f3014760bac
Tung Kah Marn <jiaming506@gmail.com>
2023-02-05 23:46:36 +0800

    26

---
+++ 7kyu/26-parts-of-list.js
@@ -0,0 +1,35 @@
+// Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+
+// Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+// Each part will be in a string
+// Elements of a pair must be in the same order as in the original array.
+// Examples of returns in different languages:
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// [["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]]
+// or
+//  a = {"az", "toto", "picaro", "zone", "kiwi"} -->
+// {{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
+// or
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+// or
+// a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
+// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+// or
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// "(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
+
+function partlist(arr) {
+  //combine arr[0 - i+1]
+  //put it into 2d array wit
+
+  return arr
+    .map(
+      (e, i) =>
+        i > arr.length - 2 || [
+          arr.slice(0, i + 1).join(" "),
+          arr.slice(i + 1).join(" "),
+        ]
+    )
+    .filter((e) => e !== true);
+}
