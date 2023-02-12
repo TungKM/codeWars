de49c2cbcff3ad3c33bc9ffd810410d32e6c87f7
Tung Kah Marn <jiaming506@gmail.com>
2023-02-12 18:27:33 +0800

    16

---
+++ 7kyu/16-fizz-buzz.js
@@ -0,0 +1,24 @@
+// DESCRIPTION:
+// Return an array containing the numbers from 1 to N, where N is the parametered value.
+
+// Replace certain values however if any of the following conditions are met:
+
+// If the value is a multiple of 3: use the value "Fizz" instead
+// If the value is a multiple of 5: use the value "Buzz" instead
+// If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+// N will never be less than 1.
+
+// Method calling example:
+
+// fizzbuzz(3) -->  [1, 2, "Fizz"]
+
+const fizzbuzz = (n) =>
+  Array.from({ length: n }, (_, index) => index + 1).map((element) =>
+    element % 15 == 0
+      ? "FizzBuzz"
+      : element % 3 == 0
+      ? "Fizz"
+      : element % 5 == 0
+      ? "Buzz"
+      : element
+  );
