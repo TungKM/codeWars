7103769948849985e4ee7a01422777451a32b3dc
Tung Kah Marn <jiaming506@gmail.com>
2023-03-13 18:52:13 +0800

    14

---
+++ 8kyu/14-remove-string-spaces.js
@@ -0,0 +1,8 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+function noSpace(x) {
+  let newStr = "";
+  for (let i = 0; i < x.length; i++) if (x[i] !== " ") newStr += x[i];
+
+  return newStr;
+}
